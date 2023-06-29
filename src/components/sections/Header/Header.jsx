00f27394f7de9000ef5c32a478d9c5486973a4ae
Alexis Quintuna <73076646+alexisquintuna@users.noreply.github.com>
2023-06-29 14:35:34 -0400

    Update Header.jsx

--- src/components/sections/Header/Header.jsx
+++ src/components/sections/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5> Hello I'm</h5>
         <h1>Alexis Quintuña</h1>
-        <h5 className="text-light">Junior Developer</h5>
+        <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <Socials />
 
